117fb77fe80fdcb26379766ec33d4e73346f1dc0
sisyfina <45711984+sisyfina@users.noreply.github.com>
2019-02-15 22:30:30 +0200

    Add files via upload

--- level3/demoAAC3.m
+++ level3/demoAAC3.m
@@ -15,8 +15,11 @@ noise2 = y1(1025:size(y2,1)+1024, 2 ) -y2 (:,2);
 %Calculate SNR for each channel
 SNR =  [ snr(y1(1025:size(y2,1)+1024,1), noise1) snr(y1(1025:size(y2,1)+1024,2), noise2) ]; 
 
-bitrate1 = audioinfo(fNameIn).BitsPerSample * audioinfo(fNameIn).SampleRate * 2 ;
-bitrate2 = audioinfo(fNameOut).BitsPerSample * audioinfo(fNameOut).SampleRate * 2 ;
+info1 = audioinfo(fNameIn) ;
+info2 = audioinfo(fNameOut) ;
+
+bitrate1 = info1.BitsPerSample * info1.SampleRate * 2 ;
+bitrate2 = info2.BitsPerSample * info2.SampleRate * 2 ;
 
 bitrate = bitrate2 ;
 compression = bitrate1 / bitrate2 ;
